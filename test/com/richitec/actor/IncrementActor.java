e54bb128ee94711694fd883003831be341d3693d
huuguanghui <huuguanghui@hotmail.com>
2012-08-15 21:28:15 +0800

    remove useless test code

--- test/com/richitec/actor/IncrementActor.java
+++ test/com/richitec/actor/IncrementActor.java
@@ -1,24 +1,24 @@
-package com.richitec.actor;
-
-import akka.actor.UntypedActor;
-
-public class IncrementActor extends UntypedActor {
-
-	@Override
-	public void onReceive(Object msg) throws Exception {
-		if (msg instanceof Integer){
-			Integer i = (Integer)msg;
-			if (0==i){
-				System.out.println("Start : " + System.currentTimeMillis());
-			}
-			i += 1;
-			if (i<1000000){
-//				Thread.sleep(5);
-				getSelf().tell(i);
-			} else {
-				System.out.println("End   : " + System.currentTimeMillis());
-			}
-		}
-	}
-
-}
+//package com.richitec.actor;
+//
+//import akka.actor.UntypedActor;
+//
+//public class IncrementActor extends UntypedActor {
+//
+//	@Override
+//	public void onReceive(Object msg) throws Exception {
+//		if (msg instanceof Integer){
+//			Integer i = (Integer)msg;
+//			if (0==i){
+//				System.out.println("Start : " + System.currentTimeMillis());
+//			}
+//			i += 1;
+//			if (i<1000000){
+////				Thread.sleep(5);
+//				getSelf().tell(i);
+//			} else {
+//				System.out.println("End   : " + System.currentTimeMillis());
+//			}
+//		}
+//	}
+//
+//}
