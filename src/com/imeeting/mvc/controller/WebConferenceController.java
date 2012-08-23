e26190d022d404d718a5fe9efcab5e300730958a
huuguanghui <huuguanghui@hotmail.com>
2012-08-23 17:29:10 +0800

    join conference from web

--- src/com/imeeting/mvc/controller/WebConferenceController.java
+++ src/com/imeeting/mvc/controller/WebConferenceController.java
@@ -1,5 +1,6 @@
 package com.imeeting.mvc.controller;
 
+
 import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpSession;
 
@@ -10,8 +11,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.imeeting.framework.ContextLoader;
+import com.imeeting.mvc.model.conference.ConferenceDB;
 import com.imeeting.mvc.model.conference.ConferenceManager;
 import com.imeeting.mvc.model.conference.ConferenceModel;
+import com.imeeting.mvc.model.conference.attendee.AttendeeModel;
 import com.imeeting.web.user.UserBean;
 
 @Controller
@@ -19,10 +22,12 @@ import com.imeeting.web.user.UserBean;
 public class WebConferenceController {
 	
 	private ConferenceManager conferenceManager;
+	private ConferenceDB conferenceDao;
 	
 	@PostConstruct
 	public void init(){
 		conferenceManager = ContextLoader.getConferenceManager();
+		conferenceDao = ContextLoader.getConferenceDAO();
 	}
 	
 	@RequestMapping(method=RequestMethod.GET)
@@ -36,12 +41,19 @@ public class WebConferenceController {
 			@RequestParam(value="confId") String confId){
 		UserBean user = (UserBean) session.getAttribute(UserBean.SESSION_BEAN);
 		ModelAndView mv = new ModelAndView();
-		ConferenceModel conference = 
-			conferenceManager.checkConferenceModel(confId, user.getName());
+		ConferenceModel conference = conferenceManager.getConference(confId);
 		if (null == conference){
 			mv.setViewName("webconf/join");
 			return mv;
 		}
+		
+		AttendeeModel attendee = new AttendeeModel(user.getName());
+		attendee.setOnlineStatus(AttendeeModel.OnlineStatus.online);
+		conference.addAttendee(attendee);
+		conferenceDao.saveAttendee(confId, user.getName());
+		
+		// notify all attendees to update attendee list
+		conference.notifyAttendeesToUpdateMemberList();
 		
 		mv.addObject("conference", conference);
 		mv.setViewName("webconf/conf");
