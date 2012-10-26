1036a81985350dc4a1561950d58c702bcbd0beeb
huuguanghui <huuguanghui@hotmail.com>
2012-10-26 21:00:59 +0800

    send conferencedestroy notify on donkey event

--- src/com/imeeting/mvc/controller/DonkeyEventController.java
+++ src/com/imeeting/mvc/controller/DonkeyEventController.java
@@ -106,6 +106,7 @@ public class DonkeyEventController {
 		}
 		conferenceManager.removeConference(requestId);
 		conferenceDao.close(requestId);
+		conferenceManager.notifyConferenceDestoryed(requestId);
 	}
 	
 	private void onConferenceStatusConflict(DonkeyEvent event){
