2be122c2496ab0b1b06022cefffe0fc69a58d284
huuguanghui <huuguanghui@hotmail.com>
2013-03-11 21:11:48 +0800

    add Email validation function

--- WebContent/WEB-INF/resources/js/applib/common.js
+++ WebContent/WEB-INF/resources/js/applib/common.js
@@ -19,6 +19,11 @@ var Util = {
 	isValidPhoneNumber : function(number) {
 		var reg = /(^[0]\d{2,3}\-\d{7,8})|(^[1-9]\d{6,7})|(^[0]\d{10,11})|(^[1][\d]{10})/;
 		return reg.exec(number);
+	},
+	
+	isValidEmail: function(email) {
+		var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+		return regex.test(email);
 	}
 
 };
